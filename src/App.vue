5fb0af917de7022ea7f180cd72544082fc5e1fba
Augusto <ampiatto@hotmail.com>
2023-05-15 21:40:27 -0300

    corrige tamanho do main e o scss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div class="app">
     <v-toolbar theme="dark">
       <DrawerIcon />
       <v-row align="center" justify="center">
         <h1>AugustinhoShop</h1>
       </v-row>
     </v-toolbar>
-    <main>
+    <main class="a__main--bodysize">
       <RouterView />
     </main>
   </div>
@@ -16,3 +16,14 @@
 import { RouterView } from 'vue-router'
 import DrawerIcon from '@/components/Drawer/DrawerIcon.vue'
 </script>
+
+<style lang="scss" scoped>
+.app {
+  .a__main--bodysize {
+    position: fixed;
+    top: 64px;
+    height: calc(100vh - 64px /* tamanho toolbar */);
+    width: 100%;
+  }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>OI</h1>
+  <v-container>
+    <v-row align="center" justify="center" class="pa-5 gap-3">
+      <mdicon name="alert" />
+      <h1>Este site não é mobile-friendly, use no seu computador</h1>
+      <!-- <h2>Temos aqui uma busca burra, fique à vontade para usar</h2> -->
+    </v-row>
+  </v-container>
 </template>

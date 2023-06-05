8df4acee130b197ee249fc5f58f77509d30a2fc1
Augusto <ampiatto@hotmail.com>
2023-06-05 02:10:17 -0300

    lazy load de componente

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
-import SecondaryPage from '../pages/SecondaryPage.vue'
+// componente fica lazy, só é carregado quando se clica para vê-lo
+const SecondaryPage = () => import(/* webpackChunkName: 'secpage' */ '../pages/SecondaryPage.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,14 +21,6 @@ const router = createRouter({
       path: '/:pathMatch(.*)*',
       component: HomePage
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ],
   scrollBehavior() {
     return window.scrollTo({ bottom: 0, behavior: 'smooth' })

1c521e47fb4f2834488155496b9f0971b164e3fc
augustopiatto <ampiatto@hotmail.com>
2023-06-26 17:35:25 -0300

    nome das paginas

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -65,6 +65,7 @@ export default {
     }
   },
   async mounted () {
+    document.title = 'Home'
     this.products = await products.getProducts()
   },
   computed: {
--- src/pages/SecondaryPage.vue
+++ src/pages/SecondaryPage.vue
@@ -33,6 +33,7 @@ export default {
   async created () {
     await this.getAPI()
     timeout
+    document.title = 'Secondary Page'
   },
   methods: {
     clearTimeout () {
